29aebe8d985b574471796fa9ffd6e36506dfd722
phdax <pophitdax@gmail.com>
2018-08-11 12:06:52 +0900

    refactor

--- bitvector/src/main/java/jp/phdax/bitvector/BitVector2.java
+++ bitvector/src/main/java/jp/phdax/bitvector/BitVector2.java
@@ -3,28 +3,30 @@ package jp.phdax.bitvector;
 public class BitVector2 {
 	
 	private final int size;
+	private final int sum;
 	private final long[] bits;
 	private final int[] rankBlock;
 	private final int[] selectBlock1;
 	private final int[] selectBlock0;
-	private final int sum;
 	
 	BitVector2(int size, long[] bits) {
+		
+		if(size < 1) throw new IllegalArgumentException("Too small size.");
+		if(bits == null || bits.length == 0) throw new IllegalArgumentException("No bits.");
+		
 		this.size = Math.min(size, bits.length << 6);
 		this.bits = bits;
 		
-		int rankBlockSize = (size >>> 6) + 1;
-		if((size & 0b111111) > 0) rankBlockSize++;
-		rankBlock = new int[rankBlockSize];
-		int nextRange = Math.min(size, 64);
-		for(int i=0; nextRange>0 && i<rankBlockSize-1; i++) {
-			rankBlock[i+1] = rankBlock[i] + Long.bitCount(bits[i] << (64 - nextRange));
-			nextRange = Math.min(size - (64 << i), 64);
+		this.rankBlock = new int[((this.size-1) >>> 6) + 2];
+		int nextRange = Math.min(size, Long.SIZE);
+		for(int i=0; nextRange>0; i++) {
+			rankBlock[i+1] = rankBlock[i] + Long.bitCount(bits[i] << (Long.SIZE - nextRange));
+			nextRange = Math.min(size - (Long.SIZE << i), Long.SIZE);
 		}
-		this.sum = rankBlock[rankBlockSize-1];
+		this.sum = rankBlock[rankBlock.length-1];
 		
-		selectBlock1 = new int[(this.sum >>> 6) + 1];
-		selectBlock0 = new int[(size - this.sum >>> 6) + 1];
+		this.selectBlock1 = new int[(this.sum >>> 6) + 1];
+		this.selectBlock0 = new int[(size - this.sum >>> 6) + 1];
 		int select1Idx = 1;
 		int select0Idx = 1;
 		for(int i=1; i<rankBlock.length; i++) {
@@ -61,7 +63,7 @@ public class BitVector2 {
 		if(pos >= size) return sum;
 		final int mod = (pos-1) & 0b111111;
 		final int idx = (pos-1) >>> 6;
-		return rankBlock[idx] + Long.bitCount(bits[idx] << 63-mod);
+		return rankBlock[idx] + Long.bitCount(bits[idx] << Long.SIZE-mod-1);
 	}
 	
 	public final int rank0(int pos) {
@@ -74,25 +76,19 @@ public class BitVector2 {
 	public final int select1(int rank) {
 		if(rank < 0 || rank > sum) return -1;
 		if(rank == 0) return 0;
-		int idx = linearSearch1(selectBlock1[rank >>> 6], rank);
-		return (idx << 6) + bitPos(bits[idx], rank-rankBlock[idx]) + 1;
+		int idx = selectBlock1[rank >>> 6];
+		for(; rankBlock[idx]<rank; idx++);
+		idx--;
+		return (idx<<6) + bitPos(bits[idx], rank-rankBlock[idx]) + 1;
 	}
 		
 	public final int select0(int rank) {
 		if(rank < 0 || rank > size-sum) return -1;
 		if(rank == 0) return 0;
-		int idx = linearSearch0(selectBlock0[rank >>> 6], rank);
-		return (idx << 6) + bitPos(~bits[idx], rank-((idx << 6) - rankBlock[idx])) + 1;
-	}
-	
-	private final int linearSearch1(int idx, int rank1) {
-		for(; rankBlock[idx]<rank1; idx++);
-		return idx-1;
-	}
-	
-	private final int linearSearch0(int idx, int rank0) {
-		for(; (idx<<6)-rankBlock[idx]<rank0; idx++);
-		return idx-1;
+		int idx = selectBlock0[rank >>> 6];
+		for(; (idx<<6)-rankBlock[idx]<rank; idx++);
+		idx--;
+		return (idx<<6) + bitPos(~bits[idx], rank-((idx<<6)-rankBlock[idx])) + 1;
 	}
 	
 	public static final int bitPos(long bits, int n) {
